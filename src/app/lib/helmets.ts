a19e87483db38e8b53bc029a5b4ed56f76ac4342
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-04 22:46:34 -0700

    Added page 2 of helmets and remaining flower helmets on page 3 of marketboard

--- src/app/lib/helmets.ts
+++ src/app/lib/helmets.ts
@@ -335,4 +335,448 @@ const helmets: helmetObj[] = [
     name: "Purple Viola Corsage",
     dyeable: false,
   },
+  {
+    name: "White Viola Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Viola Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Viola Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Pink Cherry Blossom Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Daisy Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Brightlily Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Dahlia Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Arum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Lily of the Valley Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Hydrangea Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Campanula Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Hyacinth Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Cosmos Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Morning Glory Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Red Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Chrysanthemum Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Chrysanthemum Corsage",
+    dyeable: false,
+  },
 ];
