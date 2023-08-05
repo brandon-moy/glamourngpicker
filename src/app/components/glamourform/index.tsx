55c6d093e9af1fd865f4babd453f3178f1a1fe91
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-04 22:18:18 -0700

    Minor changes - committed to work on different branch

--- src/app/components/glamourform/index.tsx
+++ src/app/components/glamourform/index.tsx
@@ -1,26 +1,62 @@
+"use client";
+import React, { useState } from "react";
+
 export default function GlamourForm() {
   return (
-    <form className="flex flex-wrap">
-      <label className="flex flex-col basis-1/2">
-        Helmet
-        <input className="w-5/6" type="number" name="helmet" min="1"></input>
+    <form className="flex flex-wrap mx-auto">
+      <label className="mb-4 text-lg font-bold basis-full">
+        Name:
+        <input
+          type="name"
+          className="w-2/5 pl-2 ml-2 font-normal"
+          name="name"
+          placeholder="Beemoy"
+        ></input>
       </label>
-      <label className="flex flex-col basis-1/2">
-        Chest
-        <input className="w-5/6" type="number" name="chest" min="1"></input>
+      <label className="flex flex-col font-bold basis-full lg:basis-1/2">
+        Helmet:
+        <input
+          className="w-full pl-2 font-normal lg:w-5/6"
+          type="number"
+          name="helmet"
+          min="1"
+        ></input>
       </label>
-      <label className="flex flex-col basis-1/2">
-        Gloves
-        <input className="w-5/6" type="number" name="gloves" min="1"></input>
+      <label className="flex flex-col font-bold basis-full lg:basis-1/2">
+        Chest:
+        <input
+          className="w-full pl-2 font-normal lg:w-5/6"
+          type="number"
+          name="chest"
+          min="1"
+        ></input>
       </label>
-
-      <label className="flex flex-col basis-1/2">
-        Legs
-        <input className="w-5/6" type="number" name="legs" min="1"></input>
+      <label className="flex flex-col font-bold basis-full lg:basis-1/2">
+        Gloves:
+        <input
+          className="w-full pl-2 font-normal lg:w-5/6"
+          type="number"
+          name="gloves"
+          min="1"
+        ></input>
       </label>
-      <label className="flex flex-col basis-1/2">
-        Boots
-        <input className="w-5/6" type="number" name="boots" min="1"></input>
+      <label className="flex flex-col font-bold basis-full lg:basis-1/2">
+        Legs:
+        <input
+          className="w-full pl-2 font-normal lg:w-5/6"
+          type="number"
+          name="legs"
+          min="1"
+        ></input>
+      </label>
+      <label className="flex flex-col font-bold basis-full lg:basis-1/2">
+        Boots:
+        <input
+          className="w-full pl-2 font-normal lg:w-5/6"
+          type="number"
+          name="boots"
+          min="1"
+        ></input>
       </label>
     </form>
   );
