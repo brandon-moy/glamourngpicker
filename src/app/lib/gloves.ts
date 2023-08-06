aefe0791f45b60b3810c524ceb47a3f124c96e67
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-06 15:17:01 -0700

    Added data for available gloves

---
+++ src/app/lib/gloves.ts
@@ -0,0 +1,136 @@
+import { gearPiece } from "./types";
+
+export const gloves: gearPiece[] = [
+  {
+    name: "Highland Mitts",
+    dyeable: true,
+  },
+  {
+    name: "Coronal Wristband",
+    dyeable: true,
+  },
+  {
+    name: "The Emperor's New Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Thavnairian Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Thavnairian Armlets",
+    dyeable: true,
+  },
+  {
+    name: "Punching Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Expeditioner's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Dinosaur Leather Gloves",
+    dyeable: true,
+  },
+  {
+    name: "New World Armlets",
+    dyeable: true,
+  },
+  {
+    name: "Griffin Leather Cuffs",
+    dyeable: true,
+  },
+  {
+    name: "Taoist's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Anemos Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Anemos Expeditionary's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Pagos Field Dressing",
+    dyeable: true,
+  },
+  {
+    name: "Pagos Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Skyworker's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Calfskin Rider's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Frontier Dress Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Peacelover's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Wristlet of Happiness",
+    dyeable: true,
+  },
+  {
+    name: "Noir Leather Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Lawless Enforcer's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Salon Server's Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Salon Server's Dress Gloves",
+    dyeable: true,
+  },
+  {
+    name: "Red Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Blue Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Yellow Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Green Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Orange Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Purple Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "White Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Black Tulip Corsage",
+    dyeable: false,
+  },
+  {
+    name: "Rainbow Tulip Corsage",
+    dyeable: false,
+  },
+];
