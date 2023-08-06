282bf4d51dcc284449345bee49535a385f625b61
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-06 16:35:49 -0700

    Added data for boots available on marketboard

---
+++ src/app/lib/boots.ts
@@ -0,0 +1,236 @@
+import { gearPiece } from "./types";
+
+export const boots: gearPiece[] = [
+  {
+    name: "Leather Duckbills",
+    dyeable: true,
+  },
+  {
+    name: "Maple Clogs",
+    dyeable: true,
+  },
+  {
+    name: "Best Man's Gaiters",
+    dyeable: true,
+  },
+  {
+    name: "Bridesmaid's Sandals",
+    dyeable: true,
+  },
+  {
+    name: "Highland Boots",
+    dyeable: true,
+  },
+  {
+    name: "Glacial Boots",
+    dyeable: true,
+  },
+  {
+    name: "Sailor Deck Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Spring Dress Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Crescent Moon Slippers",
+    dyeable: true,
+  },
+  {
+    name: "The Emperor's New Boots",
+    dyeable: false,
+  },
+  {
+    name: "Thavnairian Babouches",
+    dyeable: true,
+  },
+  {
+    name: "Thavnairian Sandals",
+    dyeable: true,
+  },
+  {
+    name: "High House Boots",
+    dyeable: true,
+  },
+  {
+    name: "High House Halfboots",
+    dyeable: true,
+  },
+  {
+    name: "Falconer's Boots",
+    dyeable: true,
+  },
+  {
+    name: "Expeditioner's Moccasins",
+    dyeable: true,
+  },
+  {
+    name: "Expeditioner's Thighboots",
+    dyeable: true,
+  },
+  {
+    name: "Dodore Boots",
+    dyeable: true,
+  },
+  {
+    name: "Dinosaur Leather Shoes",
+    dyeable: true,
+  },
+  {
+    name: "New World Moccasins",
+    dyeable: true,
+  },
+  {
+    name: "Survival Boots",
+    dyeable: true,
+  },
+  {
+    name: "Moonfire Caligae/Moonfire Sandals",
+    dyeable: true,
+  },
+  {
+    name: "Fur-lined Saurian Boots",
+    dyeable: true,
+  },
+  {
+    name: "Pteroskin Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Taoist's Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Guoc",
+    dyeable: true,
+  },
+  {
+    name: "Anemos Boots",
+    dyeable: true,
+  },
+  {
+    name: "Anemos Expeditionary's Boots",
+    dyeable: true,
+  },
+  {
+    name: "Boulevardier's Dress Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Pagos Sandals",
+    dyeable: true,
+  },
+  {
+    name: "Pagos Boots",
+    dyeable: true,
+  },
+  {
+    name: "Quaintrelle's Dress Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Whisperfine WOolen Boots",
+    dyeable: true,
+  },
+  {
+    name: "Rain Boots",
+    dyeable: true,
+  },
+  {
+    name: "Steerhide Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Urban Boots",
+    dyeable: true,
+  },
+  {
+    name: "Rebel Boots",
+    dyeable: true,
+  },
+  {
+    name: "Kupo Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Craftsman's Leather Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Damalscan Leather Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Souther Seas Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Casual Boots",
+    dyeable: true,
+  },
+  {
+    name: "Skyworker's Boots",
+    dyeable: true,
+  },
+  {
+    name: "Calfskin Rider's Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Frontier Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Frontier Pumps",
+    dyeable: true,
+  },
+  {
+    name: "Peacelover's Longboots",
+    dyeable: true,
+  },
+  {
+    name: "Boots of Happiness",
+    dyeable: true,
+  },
+  {
+    name: "Varsity Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Noir Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Lawless Enforcer's Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Salon Server's Shoes",
+    dyeable: true,
+  },
+  {
+    name: "Plain Pajama Slippers",
+    dyeable: true,
+  },
+  {
+    name: "Cactuar Pajama Slippers",
+    dyeable: true,
+  },
+  {
+    name: "Chocobo Pajama Slippers",
+    dyeable: true,
+  },
+  {
+    name: "Shishu Obutozori",
+    dyeable: true,
+  },
+  {
+    name: "Summer Sandals",
+    dyeable: false,
+  },
+  {
+    name: "Dream Boots",
+    dyeable: true,
+  },
+];
