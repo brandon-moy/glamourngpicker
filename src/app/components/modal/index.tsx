7fa401a754d1a2b2430848df03167ec68518f2e5
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-07 15:48:05 -0700

    Fixed responsive sizing on modal

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -10,14 +10,16 @@ export default function Modal() {
   if (!display) return <></>;
   return (
     <div className="absolute inset-0 bg-gray-900 bg-opacity-25">
-      <div className="w-1/2 p-8 mx-auto mt-24 bg-gray-100 rounded">
-        <p className="p-4 text-4xl text-center">Welcome to GlamouRNG Picker</p>
-        <p>
+      <div className="w-5/6 p-8 mx-auto mt-24 bg-gray-100 rounded lg:w-1/2">
+        <p className="p-4 text-2xl text-center lg:text-4xl">
+          Welcome to GlamouRNG Picker
+        </p>
+        <p className="text-sm lg:text-lg">
           Have you ever wanted a new glam but couldn&apos;t decide what you
           wanted? Well you&apos;ve come to the right place!
         </p>
         <p className="py-2 font-bold">How this works:</p>
-        <p className="pb-4">
+        <p className="pb-4 text-sm lg:text-lg">
           Currently available are all items on the Marketboard for any job at
           level 1. You just need to go through the inputs and add in a number
           within the range. To make it extra spicy from your number, it will
@@ -27,7 +29,7 @@ export default function Modal() {
           abomination to everyone&apos;s eyes!
         </p>
         <button className="font-bold" onClick={closeModal}>
-          Let&apos;s get started!
+          Get started!
         </button>
       </div>
     </div>
