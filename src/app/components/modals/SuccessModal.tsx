bdb1020c4cde74ef7c1057d1f24b02deaaf561ba
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-08 14:59:17 -0700

    Modified responsive sizing for success page to limit scrolling from users and show on one screen

--- src/app/components/modals/SuccessModal.tsx
+++ src/app/components/modals/SuccessModal.tsx
@@ -13,16 +13,19 @@ export default function SuccessModal() {
       const { name, dyeable, dye } = value;
       const itemName = capitalizeWord(item);
       return (
-        <div key={item} className="p-2 font-durusans basis-full lg:basis-1/3">
-          <p className="text-2xl font-bold font-josefinsans">{itemName}</p>
-          <p className="text-xl">{name}</p>
-          {!dyeable ? (
-            <p className="text-md">Sorry, this piece isn&apos;t dyeable</p>
-          ) : !dye.length ? (
-            <p className="text-md">No dye option was selected</p>
-          ) : (
-            <p className="text-md">{dye}</p>
-          )}
+        <div
+          key={item}
+          className="p-1 lg:p-2 font-durusans basis-full lg:basis-1/3"
+        >
+          <p className="font-bold lg:text-2xl font-josefinsans">{itemName}</p>
+          <p className="lg:text-xl">{name}</p>
+          <p className="lg:text-md">
+            {!dyeable
+              ? "Sorry, this piece isn't dyeable"
+              : !dye.length
+              ? "No dye option was selected"
+              : dye}
+          </p>
         </div>
       );
     });
@@ -37,13 +40,13 @@ export default function SuccessModal() {
   return (
     <div className="absolute inset-0 z-50 bg-gray-900 bg-opacity-25">
       <div className="w-5/6 p-8 mx-auto mt-8 bg-gray-100 rounded lg:mt-24 lg:w-1/2">
-        <p className="p-4 text-2xl font-bold text-center font-poppins lg:text-4xl">
+        <p className="p-2 text-xl font-bold text-center lg:p-4 font-poppins lg:text-4xl">
           Enjoy your brand new Glamour choice!
         </p>
         <div className="flex flex-wrap">{renderGlam(completedGlam)}</div>
         <div className="flex justify-center w-full pt-4">
           <button
-            className="text-2xl underline font-poppins"
+            className="underline lg:text-2xl font-poppins"
             onClick={restartGlamouRNG}
           >
             Restart?
--- src/app/components/modals/WelcomeModal.tsx
+++ src/app/components/modals/WelcomeModal.tsx
@@ -7,8 +7,8 @@ export default function WelcomeModal() {
 
   if (!displayWelcome) return <></>;
   return (
-    <div className="absolute inset-0 bg-gray-900 bg-opacity-25">
-      <div className="w-5/6 p-8 mx-auto mt-24 bg-gray-100 rounded lg:w-1/2">
+    <div className="absolute inset-0 z-50 bg-gray-900 bg-opacity-25">
+      <div className="w-5/6 p-8 mx-auto mt-6 bg-gray-100 rounded lg:mt-24 lg:w-1/2">
         <p className="p-4 text-2xl text-center font-poppins lg:text-4xl">
           Welcome to GlamouRNG Picker
         </p>
