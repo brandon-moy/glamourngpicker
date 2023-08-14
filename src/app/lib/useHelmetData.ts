1583fecd5fc9d143c2215da14e7d499f1cd430b4
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-14 11:05:05 -0700

    Converted handle dye functions into useHelmetData custom hooka nd utilized randomizeItemSets function to shuffle each group before picking

--- src/app/lib/useHelmetData.ts
+++ src/app/lib/useHelmetData.ts
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { fullPiece } from "./types";
 import { helmets } from "./helmets";
+import { allDyes } from "./dyes";
+import randomizeItemSets from "./randomizeItemSets";
 
 const defaultHelmet = {
   name: "",
@@ -11,4 +13,64 @@ const defaultHelmet = {
 
 export function useHelmetData() {
   const [helmet, setHelmet] = useState<fullPiece>(defaultHelmet);
+  const [invalidHelmet, setInvalidHelmet] = useState<boolean>(false);
+
+  const handleHelmetChange = (value: string) => {
+    if (value.length && (+value < 1 || +value > 213)) {
+      setInvalidHelmet(true);
+      return;
+    }
+    setInvalidHelmet(false);
+    const randomizedHelmets = randomizeItemSets(helmets);
+    const { name, dyeable } = randomizedHelmets[+value];
+    const { dyeGroup, dye } = helmet;
+    const updatedHelmet = {
+      name,
+      dyeable,
+      dyeGroup,
+      dye,
+    };
+    if (!dyeable) {
+      updatedHelmet.dyeGroup = 0;
+      updatedHelmet.dye = "";
+    }
+    setHelmet(updatedHelmet);
+  };
+
+  const handleHelmetDyeGroup = (value: string) => {
+    if (value.length && (+value < 1 || +value > allDyes.length)) {
+      setInvalidHelmet(true);
+      return;
+    }
+    setInvalidHelmet(false);
+    const randomizedDyeGroup = randomizeItemSets(allDyes);
+    const dyeGroup = randomizedDyeGroup[+value];
+    const { name, dyeable, dye } = helmet;
+    const updatedHelmet = {
+      name,
+      dyeable,
+      dyeGroup,
+      dye,
+    };
+    setHelmet(updatedHelmet);
+  };
+
+  const handleHelmetDyeColor = (value: string) => {
+    const dyeIndex = helmet.dyeGroup;
+    if (value.length && (+value < 1 || +value > allDyes[dyeIndex].length)) {
+      setInvalidHelmet(true);
+      return;
+    }
+    setInvalidHelmet(false);
+    const randomizedDyes = randomizeItemSets(allDyes[dyeIndex]);
+    const dye = randomizedDyes[+value];
+    const { name, dyeable, dyeGroup } = helmet;
+    const updatedHelmet = {
+      name,
+      dyeable,
+      dyeGroup,
+      dye,
+    };
+    setHelmet(updatedHelmet);
+  };
 }
